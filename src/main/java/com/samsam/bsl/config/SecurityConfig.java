b11883d04940e2777a8ede5055e8bc654981930b
mink <use1017@naver.com>
2023-09-18 17:50:20 +0900

    passwordEncoder

--- src/main/java/com/samsam/bsl/config/SecurityConfig.java
+++ src/main/java/com/samsam/bsl/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,5 +22,11 @@ public class SecurityConfig {
       .csrf().disable();
 
     return http.build();
+  }
+  
+  //비밀번호 암호화
+  @Bean
+  public BCryptPasswordEncoder encodePwd() {
+	  return new BCryptPasswordEncoder();
   }
 }
