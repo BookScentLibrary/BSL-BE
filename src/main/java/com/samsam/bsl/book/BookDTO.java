ab34b8932f3f50ffbfa53c5c1101ab68fcc418d8
yuna <playamongthestars000@gmail.com>
2023-10-04 17:48:23 +0900

    error fix

--- src/main/java/com/samsam/bsl/book/BookDTO.java
+++ src/main/java/com/samsam/bsl/book/BookDTO.java
@@ -1,11 +1,8 @@
 package com.samsam.bsl.book;
 
-import org.springframework.stereotype.Repository;
-
 import lombok.Getter;
 import lombok.Setter;
 
-@Repository
 @Getter
 @Setter
 public class BookDTO {
@@ -17,8 +14,8 @@ public class BookDTO {
 	  private String publicationYear;
 	  private String callNum;
 	  private String shelfarea;
-	  private String bookformat;
-	  private String classname;
+	  private String format;
+	  private String className;
 	  private int bookStatus;
 	  private int rentCnt;
 	  private String isbn;
