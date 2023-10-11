6c42b039dde8aaaa4d25405333e0e97ac87bda6c
nongbu24 <pop524@naver.com>
2023-10-11 17:24:54 +0900

    feat #2 odd code

--- src/main/java/com/samsam/bsl/newbooks/NewBooksService.java
+++ src/main/java/com/samsam/bsl/newbooks/NewBooksService.java
@@ -1,5 +1,6 @@
 package com.samsam.bsl.newbooks;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -8,6 +9,7 @@ public class NewBooksService {
 	
     private final NewBooksRepository newBooksRepository;
 
+    @Autowired
     public NewBooksService(NewBooksRepository newBooksRepository) {
         this.newBooksRepository = newBooksRepository;
     }
