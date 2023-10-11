97e47c8fec51de02319dd66e216359e4ab5ac356
yuna <playamongthestars000@gmail.com>
2023-10-11 18:22:40 +0900

    merge: 팀원 코드 병합

--- src/main/java/com/samsam/bsl/book/rent/domain/Book.java
+++ src/main/java/com/samsam/bsl/book/rent/domain/Book.java
@@ -5,11 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
