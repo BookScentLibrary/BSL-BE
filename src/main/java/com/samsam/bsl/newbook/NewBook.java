cbec5dd7632fcdb7c6764deb845ef16e138561f4
nongbu24 <pop524@naver.com>
2023-10-13 22:58:45 +0900

    feat code correction

--- src/main/java/com/samsam/bsl/newbook/NewBook.java
+++ src/main/java/com/samsam/bsl/newbook/NewBook.java
@@ -6,19 +6,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Getter;
-import lombok.Setter;
-
 @Entity
-@Getter
-@Setter
 @Table(name="newbook")
 public class NewBook {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int booknum;
-	private String bookname;
-	private String author;
-	private String publisher;
-	private String regDate;
 }
--- src/main/java/com/samsam/bsl/newbook/NewBookService.java
+++ src/main/java/com/samsam/bsl/newbook/NewBookService.java
@@ -1,6 +1,5 @@
 package com.samsam.bsl.newbook;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -9,7 +8,6 @@ public class NewBookService {
 	
     private final NewBookRepository newBookRepository;
 
-    @Autowired
     public NewBookService(NewBookRepository newBookRepository) {
         this.newBookRepository = newBookRepository;
     }
