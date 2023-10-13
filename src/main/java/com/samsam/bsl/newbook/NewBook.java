93fc1bd3b02d7d1925a6c9d6149c15d3ec74bbe9
nongbu24 <pop524@naver.com>
2023-10-14 00:20:47 +0900

    sql syntax error

--- src/main/java/com/samsam/bsl/newbook/NewBook.java
+++ src/main/java/com/samsam/bsl/newbook/NewBook.java
@@ -6,10 +6,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
-@Table(name="newbook")
+@Getter
+@Setter
+@Table(name="newBook")
 public class NewBook {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int booknum;
+	private int bookNo;
 }
