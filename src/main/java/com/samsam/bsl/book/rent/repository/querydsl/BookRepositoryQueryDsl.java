0f6393f61f98bfe484cb07d73ebd0a70ba92de61
river <playamongthestars000@gmail.com>
2023-10-15 17:31:58 +0900

    feat #9 : 도서 반납시 도서 대출 내역 테이블 업데이트, 리뷰 조회 완료

--- src/main/java/com/samsam/bsl/book/rent/repository/querydsl/BookRepositoryQueryDsl.java
+++ src/main/java/com/samsam/bsl/book/rent/repository/querydsl/BookRepositoryQueryDsl.java
@@ -3,11 +3,15 @@ package com.samsam.bsl.book.rent.repository.querydsl;
 import com.samsam.bsl.book.rent.domain.Book;
 import com.samsam.bsl.book.rent.domain.Rate;
 import com.samsam.bsl.book.rent.domain.Reader;
+import com.samsam.bsl.book.review.domain.Review;
+import com.samsam.bsl.book.review.dto.ReviewDTO;
 import com.samsam.bsl.user.entity.UserEntity;
+
+import java.util.List;
 
 public interface BookRepositoryQueryDsl {
     Book getBook(int bookNo);
     Rate getRate(int bookNo);
     Reader getReader(int bookNo);
-    UserEntity getUserInfo(String username);
+    List<ReviewDTO> getReview(int bookNo);
 }
